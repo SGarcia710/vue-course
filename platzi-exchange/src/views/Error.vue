3897d56f7bb23f864c7843c6a1d4002e2fdae3d9
SGarcia710 <sgarcia710@outlook.com>
2020-03-10 00:35:34 -0500

    Basic project Started

---
+++ platzi-exchange/src/views/Error.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl uppercase">404 not found</h1>
+    <router-link class="mt-5 text-xl text-green-600 hover:underline" to="/"
+      >Go back to home</router-link
+    >
+  </div>
+</template>
---
+++ platzi-exchange/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="">
+    <!-- I can avoid writing v-bind: just using : as prefix of the property -->
+    <px-assets-table :assets="assets" />
+  </div>
+</template>
+
+<script>
+import api from '@/api';
+import PxAssetsTable from '@/components/PxAssetsTable';
+
+export default {
+  name: 'Home',
+
+  components: { PxAssetsTable },
+
+  data() {
+    return {
+      assets: []
+    };
+  },
+
+  created() {
+    api.getAssets().then(assets => (this.assets = assets));
+  }
+};
+</script>
